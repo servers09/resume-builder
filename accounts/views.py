eeab53098f87987fd71f8d39eb4402df88045116
Vince <vrefamonte@gmail.com>
2019-02-13 18:06:06 +0800

    created user login

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,19 @@ from .forms import LoginForm ,UserRegisterForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate , logout
 
+class UserRegister(TemplateView):
+	template_name = 'accounts/register.html'
+	def post(self,*args,**kwargs):
+		form = UserRegisterForm(self.request.POST)
+		if form.is_valid():
+			form.save()
+			username = form.cleaned_data.get('username')
+			messages.success(self.request, f'Account created for {username}!')
+			return redirect('/accounts/login')
+		return render(self.request, self.template_name, {'form': form})
+	def get(self,*args,**kwargs): 
+		form = UserRegisterForm()
+		return render(self.request, self.template_name, {'form': form})
 
 class UserLogin(TemplateView):
 	template_name = 'accounts/login.html'
