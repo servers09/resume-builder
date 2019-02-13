ec2b1342e0390af0c9ff21f3cec55aa4e808cbce
Vince <vrefamonte@gmail.com>
2019-02-13 09:41:07 +0800

    created user login

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 app_name = 'accounts'
 urlpatterns = [
+    path('login/', views.UserLogin.as_view(),name='login'),
     
-    path('register/', views.UserRegister.as_view(),name='register'),
     ]
--- accounts/views.py
+++ accounts/views.py
@@ -5,17 +5,18 @@ from .forms import LoginForm ,UserRegisterForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate , logout
 
-
-class UserRegister(TemplateView):
-	template_name = 'accounts/register.html'
+class UserLogin(TemplateView):
+	template_name = 'accounts/login.html'
 	def post(self,*args,**kwargs):
-		form = UserRegisterForm(self.request.POST)
-		if form.is_valid():
-			form.save()
-			username = form.cleaned_data.get('username')
-			messages.success(self.request, f'Account created for {username}!')
-			return redirect('/accounts/login')
-		return render(self.request, self.template_name, {'form': form})
-	def get(self,*args,**kwargs): 
-		form = UserRegisterForm()
-		return render(self.request, self.template_name, {'form': form})
+		forms = LoginForm(self.request.POST)
+		if forms.is_valid():
+			user = authenticate(username=forms.cleaned_data.get('username')
+        		,password=forms.cleaned_data.get('password'))
+			login(self.request,user)
+			return redirect('/builder/edit/'+str(self.request.user.id)+'/')
+
+	def get(self,*args,**kwargs):
+		forms = LoginForm()
+		return render(self.request, self.template_name, {'forms': forms,})
+
+
