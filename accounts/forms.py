66c50c78af915f70ac6c0661b9d71e36e354af9e
Vince <vrefamonte@gmail.com>
2019-02-14 13:10:29 +0800

    applied corrections on user registration

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,12 +2,12 @@ from django.contrib.auth.models import User
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 
+
 class UserRegisterForm(UserCreationForm):
+
 	class Meta:
 		model = User
 		fields = ('username','email','password1','password2','first_name','last_name')
 
-class LoginForm(forms.Form):
-	username = forms.CharField(widget = forms.TextInput())
-	password = forms.CharField(widget = forms.TextInput(attrs={'type':'password'}))
+
 
--- accounts/views.py
+++ accounts/views.py
@@ -1,40 +1,25 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.contrib import messages
-from .forms import LoginForm ,UserRegisterForm
+from .forms import UserRegisterForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate , logout
+from django.urls import reverse
 
 class UserRegister(TemplateView):
 	template_name = 'accounts/register.html'
+
 	def post(self,*args,**kwargs):
 		form = UserRegisterForm(self.request.POST)
 		if form.is_valid():
 			form.save()
 			username = form.cleaned_data.get('username')
 			messages.success(self.request, f'Account created for {username}!')
-			return redirect('/accounts/login')
+			return redirect(reverse('login'))
 		return render(self.request, self.template_name, {'form': form})
+
 	def get(self,*args,**kwargs): 
 		form = UserRegisterForm()
 		return render(self.request, self.template_name, {'form': form})
 
-class UserLogin(TemplateView):
-	template_name = 'accounts/login.html'
-	def post(self,*args,**kwargs):
-		forms = LoginForm(self.request.POST)
-		if forms.is_valid():
-			user = authenticate(username=forms.cleaned_data.get('username')
-        		,password=forms.cleaned_data.get('password'))
-			login(self.request,user)
-			return redirect('/builder/edit/'+str(self.request.user.id)+'/')
 
-	def get(self,*args,**kwargs):
-		forms = LoginForm()
-		return render(self.request, self.template_name, {'forms': forms,})
-
-class Logout(TemplateView):
-	template_name = 'accounts/logout.html'
-	def get(self,*args,**kwargs):
-		logout(self.request)
-		return render(self.request, self.template_name, )
