e77b0ac41d53f4322cbf22fd675325c4a4cdd907
Vince <vrefamonte@gmail.com>
2019-02-14 18:07:27 +0800

    modified user login validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,50 @@
 from django.contrib.auth.models import User
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+from django.contrib.auth import authenticate
 
 class UserRegisterForm(UserCreationForm):
+
 	class Meta:
 		model = User
-		fields = ('username','email','password1','password2','first_name','last_name')
+		fields = ('username','email','password1','password2',)
+
+	def clean_email(self):
+		email = User.objects.filter(email=self.cleaned_data['email'])
+		if email.exists():
+			raise forms.ValidationError('Email already exists!')
+		return self.cleaned_data['email']
+
+	def clean_username(self):
+		username = User.objects.filter(username=self.cleaned_data['username'])
+		if username.exists():
+			raise forms.ValidationError('Username already exists!')
+		return self.cleaned_data['username']
+
+	def clean(self):
+		password1 = self.cleaned_data.get('password1')
+		password2 = self.cleaned_data.get('password2')
+
+		if password1 != password2:
+			raise forms.ValidationError('Passwords did not match!')
+			return self.cleaned_data
+
 
 class LoginForm(forms.Form):
 	username = forms.CharField(widget = forms.TextInput())
 	password = forms.CharField(widget = forms.TextInput(attrs={'type':'password'}))
+
+	class Meta:
+		model = User
+		fields = ('username','password',)
+
+	def clean(self):
+		username = self.cleaned_data['username']
+		password = self.cleaned_data['password']
+
+		user = authenticate(username=username,password=password)
+
+		if user is not None:
+			return self.cleaned_data
+		raise forms.ValidationError('Invalid Username or Password')
--- accounts/views.py
+++ accounts/views.py
@@ -1,33 +1,64 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.contrib import messages
-from .forms import LoginForm ,UserRegisterForm
+from .forms import UserRegisterForm, LoginForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate , logout
+from django.urls import reverse
+
 
 class UserRegister(TemplateView):
+	"""
+	Register View
+	GET - get forms and render template
+	POST - register user account
+	"""
 	template_name = 'accounts/register.html'
+
 	def post(self,*args,**kwargs):
-		form = UserRegisterForm(self.request.POST)
-		if form.is_valid():
-			form.save()
-			username = form.cleaned_data.get('username')
-			messages.success(self.request, f'Account created for {username}!')
+		forms = UserRegisterForm(self.request.POST)
+		
+		if forms.is_valid():
+			user = forms.save()
+			user.set_password(form.cleaned_data.get('password'))
+			user.save()
+
+			user = authenticate(
+					username=forms.cleaned_data.get('username'),
+					password=forms.cleaned_data.get('password')
+					)
+			messages.success(self.request, f'Account Created you may now Log in!')
 			return redirect('/accounts/login')
+		else:
+			print(form.errors)
+
 		return render(self.request, self.template_name, {'form': form})
+
 	def get(self,*args,**kwargs): 
 		form = UserRegisterForm()
 		return render(self.request, self.template_name, {'form': form})
 
+
+
 class UserLogin(TemplateView):
+	"""
+	Login View
+	GET - get forms and render template
+	POST - login user
+	"""
 	template_name = 'accounts/login.html'
+
 	def post(self,*args,**kwargs):
 		forms = LoginForm(self.request.POST)
+
 		if forms.is_valid():
-			user = authenticate(username=forms.cleaned_data.get('username')
-        		,password=forms.cleaned_data.get('password'))
+			user = authenticate(username=forms.cleaned_data.get('username'),password=forms.cleaned_data.get('password'))
 			login(self.request,user)
-			return redirect('/builder/edit/'+str(self.request.user.id)+'/')
+			return redirect('/builder/edit')
+		else:
+			print(forms.errors)
+
+		return render(self.request, self.template_name, {'forms': forms})
 
 	def get(self,*args,**kwargs):
 		forms = LoginForm()
