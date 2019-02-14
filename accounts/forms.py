8fda79ca656db1a56be8995535b4a3440574d6d3
Vince <vrefamonte@gmail.com>
2019-02-14 14:07:04 +0800

    removed first-name and last-name from register fields

--- accounts/forms.py
+++ accounts/forms.py
@@ -7,7 +7,7 @@ class UserRegisterForm(UserCreationForm):
 
 	class Meta:
 		model = User
-		fields = ('username','email','password1','password2','first_name','last_name')
+		fields = ('username','email','password1','password2')
 
 
 
